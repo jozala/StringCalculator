d48504b587057c76157d3a44647400eaadf7c30c
mariusz <mariusz@aetas.pl>
2014-12-16 22:54:58 +0000

    Return 0 when empty string scenario implemented

--- src/main/java/pl/aetas/oakfusion/StringCalculator.java
+++ src/main/java/pl/aetas/oakfusion/StringCalculator.java
@@ -2,6 +2,9 @@ package pl.aetas.oakfusion;
 
 public class StringCalculator {
 
+    public int add(String inputString) {
+        return 0;
+    }
 }
 
 
--- src/test/java/pl/aetas/oakfusion/StringCalculatorTest.java
+++ src/test/java/pl/aetas/oakfusion/StringCalculatorTest.java
@@ -1,5 +1,23 @@
 package pl.aetas.oakfusion;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StringCalculatorTest {
 
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void setUp() throws Exception {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void shouldReturnZeroWhenGivenEmptyString() throws Exception {
+        int result = stringCalculator.add("");
+        assertThat(result, is(0));
+    }
 }
